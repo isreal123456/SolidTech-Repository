d8c19ce55f967e903c0b74ab0caaba8da5f08378
isreal123456 <120799006+isreal123456@users.noreply.github.com>
2024-06-10 21:52:58 +0100

    ss

--- utils/context_processors.py
+++
@@ -1,9 +0,0 @@
-from Sellers.models import Log
-from Warehouse.models import Category
-from mptt.templatetags.mptt_tags import full_tree_for_model
-
-def navbar_categories(request):
-    categories = Category.objects.all()
-    category = Category.objects.filter(parent=None)
-    log = Log.objects.all().order_by('-id')[:5]
-    return {'navbar_categories':categories,'category':category,'log':log}
