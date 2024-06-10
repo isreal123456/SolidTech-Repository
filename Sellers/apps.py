04ed29bad3e231d9d9e60fc1539b4bd715e6db7c
isreal123456 <120799006+isreal123456@users.noreply.github.com>
2024-06-10 21:57:52 +0100

    as

--- Sellers/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class SellersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Sellers'
-
-    def ready(self):
-        import Sellers.signals
